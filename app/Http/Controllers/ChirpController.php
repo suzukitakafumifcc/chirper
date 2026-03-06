df90a2bbee48609653207d4c21f2609ed556bb8f
SuzukiTakafumi <suzukitakafumifcc@gmail.com>
2026-03-06 10:47:06 +0900

    feat: ChirpController/index
    ビューへのデータ渡し

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -8,6 +8,24 @@ class ChirpController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $chirps = [
+            [
+                'author' => 'Jane Doe',
+                'message' => 'Just deployed my first Laravel app! 🚀',
+                'time' => '5 minutes ago'
+            ],
+            [
+                'author' => 'John Smith',
+                'message' => 'Laravel makes web development fun again!',
+                'time' => '1 hour ago'
+            ],
+            [
+                'author' => 'Alice Johnson',
+                'message' => 'Working on something cool with Chirper...',
+                'time' => '3 hours ago'
+            ]
+        ];
+
+        return view('home', ['chirps' => $chirps]);
     }
 }
