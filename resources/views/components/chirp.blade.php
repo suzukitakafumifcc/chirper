aab1d595e29307cedc38d818418afd1c955997d9
SuzukiTakafumi <suzukitakafumifcc@gmail.com>
2026-03-06 14:01:38 +0900

    feat: ChirpSeeder

--- resources/views/components/chirp.blade.php
+++ resources/views/components/chirp.blade.php
@@ -6,14 +6,14 @@
             @if ($chirp->user)
                 <div class="avatar">
                     <div class="size-10 rounded-full">
-                        <img src="<https://avatars.laravel.cloud/>{{ urlencode($chirp->user->email) }}"
+                        <img src="https://avatars.laravel.cloud/{{ urlencode($chirp->user->email) }}"
                             alt="{{ $chirp->user->name }}'s avatar" class="rounded-full" />
                     </div>
                 </div>
             @else
                 <div class="avatar placeholder">
                     <div class="size-10 rounded-full">
-                        <img src="<https://avatars.laravel.cloud/f61123d5-0b27-434c-a4ae-c653c7fc9ed6?vibe=stealth>"
+                        <img src="https://avatars.laravel.cloud/f61123d5-0b27-434c-a4ae-c653c7fc9ed6?vibe=stealth"
                             alt="Anonymous User" class="rounded-full" />
                     </div>
                 </div>
