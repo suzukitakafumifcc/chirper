55ac1158273ed9a42fac2e157686d429dfc1cad8
SuzukiTakafumi <suzukitakafumifcc@gmail.com>
2026-03-06 14:24:05 +0900

    feat: webルート
    chipリソースルートの追加

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ChirpController::class, 'index']);
 Route::post('/chirps', [ChirpController::class, 'store']);
+Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
