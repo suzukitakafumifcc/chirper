e342acea28f5ce1f732e77c6bd15e971a48da563
SuzukiTakafumi <suzukitakafumifcc@gmail.com>
2026-03-06 11:22:50 +0900

    feat: homeビュー
    モデルへの対応

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,9 +8,13 @@
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
                     <div>
-                        <div class="font-semibold">{{ $chirp['author'] }}</div>
-                        <div class="mt-1">{{ $chirp['message'] }}</div>
-                        <div class="text-sm text-gray-500 mt-2">{{ $chirp['time'] }}</div>
+                        <div class="font-semibold">
+                            {{ $chirp->user ? $chirp->user->name : 'Anonymous' }}
+                        </div>
+                        <div class="mt-1">{{ $chirp->message }}</div>
+                        <div class="text-sm text-gray-500 mt-2">
+                            {{ $chirp->created_at->diffForHumans() }}
+                        </div>
                     </div>
                 </div>
             </div>
