19c3ebfb4974740bea4f77903e5c11334f201323
SuzukiTakafumi <suzukitakafumifcc@gmail.com>
2026-03-07 09:30:14 +0900

    feat: webルート
    ユーザー登録ルート追加

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
+use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/', [ChirpController::class, 'index']);
 
 Route::resource('chirps', ChirpController::class)
     ->only(['store', 'edit', 'update', 'destroy']);
+
+// Registration routes
+Route::view('/register', 'auth.register')
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register', Register::class)
+    ->middleware('guest');
