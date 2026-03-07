cefdf2e1a439ad4e4c8a331167b1478428a315c9
SuzukiTakafumi <suzukitakafumifcc@gmail.com>
2026-03-07 09:12:06 +0900

    feat: chirpコンポーネント
    更新通知の追加

--- resources/views/components/chirp.blade.php
+++ resources/views/components/chirp.blade.php
@@ -24,6 +24,10 @@
                     <span class="text-sm font-semibold">{{ $chirp->user ? $chirp->user->name : 'Anonymous' }}</span>
                     <span class="text-base-content/60">·</span>
                     <span class="text-sm text-base-content/60">{{ $chirp->created_at->diffForHumans() }}</span>
+                    @if ($chirp->updated_at->gt($chirp->created_at->addSeconds(5)))
+                        <span class="text-base-content/60">·</span>
+                        <span class="text-sm text-base-content/60 italic">edited</span>
+                    @endif
                 </div>
 
                 <p class="mt-1">
